585d3dd7965416f9c044ca05c268238b802004d0
Rodrigo de Souza <rsouza01@gmail.com>
2020-06-06 22:59:50 +0200

    Tests enabled

--- test/tokenizer/parser.unit.ts
+++ test/tokenizer/parser.unit.ts
@@ -69,11 +69,11 @@ describe('Parser', () => {
     expect(astRet).to.be.eql(ast);
   });
 
-  it.only('Must throw exception for rule  incorrectly defined - $deviceGroup<>\'ABC143\'', async () => {
+  it('Must throw exception for rule  incorrectly defined - $deviceGroup<>\'ABC143\'', async () => {
     const rule = "$deviceGroup<>'ABC143'";
     const astRet = new Parser().parse(rule);
 
-    console.log(`>>> astRet2: ${JSON.stringify(astRet, null, 2)}`);
+    // console.log(`>>> astRet2: ${JSON.stringify(astRet, null, 2)}`);
 
     // expect(astRet).to.be.eql(ast);
   });
