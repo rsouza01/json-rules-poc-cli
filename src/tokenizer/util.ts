5a0b8f90cbe72f3a8aad925c44a76eece7ea88fb
Rodrigo de Souza <rsouza01@gmail.com>
2020-06-06 13:17:29 +0200

    ESLint fixed

--- src/tokenizer/util.ts
+++ src/tokenizer/util.ts
@@ -1,17 +1,17 @@
 // util.js
 export function isNum(v) {
-  return !isNaN(parseFloat(v)) && isFinite(v)
+  return !isNaN(parseFloat(v)) && isFinite(v);
 }
 
 export function isVariable(v) {
-  return !isNaN(parseFloat(v)) && isFinite(v)
+  return !isNaN(parseFloat(v)) && isFinite(v);
 }
 
 // util.js
-const operators = ['=', '+', '-', '*', '/', '>', '<', '>=', '<=', '==', '!=', '&', '|', 'in']
+const operators = ['=', '+', '-', '*', '/', '>', '<', '>=', '<=', '==', '!=', '&', '|', 'in'];
 export function isOp(v) {
-  for (var i = 0; i < operators.length; i++) {
-      if (operators[i] == v) return true;
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] === v) return true;
   }
-  return false
+  return false;
 }
