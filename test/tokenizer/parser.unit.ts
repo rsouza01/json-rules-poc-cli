f89fb515ff87608276f149b1d3b2eae6cc8aad83
Rodrigo de Souza <rsouza01@gmail.com>
2020-06-06 19:54:42 +0200

    New test added

--- test/tokenizer/parser.unit.ts
+++ test/tokenizer/parser.unit.ts
@@ -1,62 +1,66 @@
+import {
+  describe, it
+} from 'mocha';
+import * as chai from 'chai';
+
 import { Parser } from '../../src/tokenizer/parser';
 import { Token } from '../../src/tokenizer/token';
 
+const { expect } = chai;
+
+
 describe('Parser', () => {
-  const rule = "$deviceGroup='ABC143'";
-  const ruleOR = "$deviceGroup='ABC143' | $deviceType='TV'";
-
-  const validTokensOR = [
-    {
-      type: 'VAR',
-      value: '$deviceGroup'
-    },
-    {
-      type: 'OP',
-      value: '='
-    },
-    {
-      type: 'VAR_VALUE',
-      value: "'ABC143'"
-    },
-    {
-      type: 'OP',
-      value: '|'
-    },
-    {
-      type: 'VAR',
-      value: '$deviceType'
-    },
-    {
-      type: 'OP',
-      value: '='
-    },
-    {
-      type: 'VAR_VALUE',
-      value: "'TV'"
-    },
-    {
-      type: 'EOF'
-    }
-  ];
-  /*
-AST: [
-  {
-    "left": {
-      "value": "12"
-    },
-    "right": {
-      "value": "3"
-    },
-    "operator": "ADD"
-  }
-]
-*/
-
   describe('Parser', () => {
-    it.only('Parse rule correctly defined - $deviceGroup=\'ABC143\'', () => {
-      const ast = new Parser().parse(rule);
-      // const ast = new Parser().parse('12+3');
-      console.log(`AST: ${JSON.stringify(ast, null, 2)}`);
+    it('Parse rule correctly defined - $deviceGroup=\'ABC143\'', () => {
+      const ast = [
+        {
+          left: {
+            value: '$deviceGroup'
+          },
+          right: {
+            value: "'ABC143'"
+          },
+          operator: 'EQUAL_EQUAL'
+        }
+      ];
+
+      const rule = "$deviceGroup='ABC143'";
+      const astRet = new Parser().parse(rule);
+
+      expect(astRet).to.be.eql(ast);
     });
+  });
+
+  it.only('Parse rule correctly defined - $deviceGroup=\'ABC143\'', () => {
+    const ast = [
+      {
+        left: {
+          value: '$deviceGroup'
+        },
+        right: {
+          left: {
+            value: "'ABC143'"
+          },
+          right: {
+            left: {
+              value: '$deviceType'
+            },
+            right: {
+              value: "'TV'"
+            },
+            operator: 'EQUAL_EQUAL'
+          },
+          operator: 'OR'
+        },
+        operator: 'EQUAL_EQUAL'
+      }
+    ];
+    const rule = "$deviceGroup='ABC143' | $deviceType='TV'";
+
+    const astRet = new Parser().parse(rule);
+
+    console.log(`astRet = ${JSON.stringify(astRet, null, 2)}`);
+
+    // expect(astRet).to.be.eql(ast);
   });
 });
